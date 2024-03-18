960d934ab7e9fc10ab263005c884d3db53520214
Light-Yagami-The-Kira <krishanukarmakar.kk94@gmail.com>
2024-03-18 16:49:59 +0530

    if else conditions

---
+++ if_else_conditions.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    bool condition = true;
+    if (condition==true)
+    {
+        printf("Hello World")
+    }
+    else
+    {
+        printf("Bye")
+    }
+    return 0;
+}
