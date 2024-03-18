7fa95f0ff9fe25b16b770d92561099f6a207d473
Light-Yagami-The-Kira <krishanukarmakar.kk94@gmail.com>
2024-03-18 17:02:23 +0530

    Type Casting

---
+++ typecasting.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    float a = 4.5;
+    /*
+    TYPECASTING SYNATX:
+    (datatype) value_or_identifier
+    */
+    printf("%d", (int)a );
+    return 0;
+}
